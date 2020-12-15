c32bf111f561ef1dadbc14f01e2f1d042faa8d52
Fred J Hickernell <hickernell@iit.edu>
2020-12-15 07:17:57 -0600

    Update from office computer

---
+++ ags/python/first_Lattice.py
@@ -0,0 +1,12 @@
+>> lattice = qp.Lattice(dimenaion=2,randomize=False)
+>> lattice.gen_samples(n=8)
+/usr/local/lib/python3.6/dist-packages/qmcpy/discrete_distribution/lattice/lattice.py:197: ParameterWarning: Non-randomized lattice sequence includes the origin
+  warnings.warn("Non-randomized lattice sequence includes the origin",ParameterWarning)
+array([[0.   , 0.   ],
+       [0.5  , 0.5  ],
+       [0.25 , 0.75 ],
+       [0.75 , 0.25 ],
+       [0.125, 0.875],
+       [0.625, 0.375],
+       [0.375, 0.625],
+       [0.875, 0.125]])
---
+++ ags/python/first_Sobol.py
@@ -0,0 +1,2 @@
+!pip install qmcpy==0.8.1a0 # install qmcpy
+import qmcpy as qp # bring qmcpy into the environment 
