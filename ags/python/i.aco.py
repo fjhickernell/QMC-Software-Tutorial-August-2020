fdb259b000115fad9b31bfea4e8af87a15b32958
fjhickernell <hickernell@iit.edu>
2020-12-16 12:44:13 -0600

    clean up components sections

--- ags/python/i.aco.py
+++ ags/python/i.aco.py
@@ -1,7 +1,12 @@
->>> dd = qp.Sobol(4,seed=7)
->>> m = qp.BrownianMotion(dd)
->>> ac = qp.AsianOption(m)
->>> x = dd.gen_samples(2**10)
->>> y = ac.f(x)
+>>> sobol = qp.Sobol(4) # t = [1/4, 1/2, 3/4, 1]
+>>> aco = qp.AsianOption(
+...         qp.BrownianMotion(sobol, drift=2), 
+...         start_price = 30,
+...         strike_price = 45,
+...         volatility = 0.5,
+...         interest_rate = 0,
+...         call_put = 'call')
+>>> x = sobol.gen_samples(2**10)
+>>> y = aco.f(x)
 >>> y.mean()
-FIX CODE with start price, strike price, call option, ...
+0.4349545319177899
