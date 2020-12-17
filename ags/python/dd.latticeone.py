89e88a38b19d76476a5b3718d4c710e910ba67b5
Fred J Hickernell <hickernell@iit.edu>
2020-12-16 18:26:49 -0600

    Update from office computer

---
+++ ags/python/dd.latticeone.py
@@ -0,0 +1,4 @@
+>>> lattice = qp.Lattice(2); lattice.gen_samples(1)
+array([[0.47648752 0.53062561]])
+>>> lattice.gen_samples(1)
+array([[0.47648752 0.53062561]])
--- ags/python/dd.sobol.py
+++ ags/python/dd.sobol.py
@@ -1,6 +1,8 @@
 >>> sobol = qp.Sobol(2,randomize=False)
->>> sobol.gen_samples(n_min=4,n_max=8,warn=False)
-array([[0.25 , 0.75 ],
+>>> sobol.gen_samples(8,warn=False)
+array([[0.   , 0.   ],
+       [0.5  , 0.5  ],
+       [0.25 , 0.75 ],
        [0.75 , 0.25 ],
        [0.125, 0.625],
        [0.625, 0.125],
