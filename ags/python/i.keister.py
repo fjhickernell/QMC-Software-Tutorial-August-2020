9fa99b2f0ea36c4cae0ca9ce6a65f4e1d74e0f37
Fred Hickernell <hickernell@iit.edu>
2020-12-19 17:58:32 -0600

    Updates from Home Computer

--- ags/python/i.keister.py
+++ ags/python/i.keister.py
@@ -1,16 +1,10 @@
->>> import numpy as np
->>> d = 2 # dimension
 >>> def my_Keister(x): # create a custom function
+...	d = x.shape[1]
 ...     norm = np.sqrt((x**2).sum(1)) # sum along rows
-...     k = np.pi**(d/2)*np.cos(norm)
-...     return k  # size n vector
->>> # define QMCPy components
->>> gaussian = qp.Gaussian(qp.Halton(d), 
-                    mean = 0, 
-                    covariance = 1/2)
->>> cf = qp.CustomFun(gaussian, custom_fun=my_Keister)
->>> # Use QMCPy objects to evaluate integrand
->>> x = cf.distribution.gen_samples(2**7) # generate Halton samples
->>> y = cf.f(x) # evaluate transformed integrand at Halton points
->>> y.mean() # approximate the mean of the distribution
+...     out = np.pi**(d/2)*np.cos(norm)
+...     return out  # size n vector
+>>> gaussian = qp.Gaussian(qp.Halton(d), mean = 0, covariance = 1/2) # define a Gaussian measure based on Halton
+>>> cf = qp.CustomFun(gaussian, custom_fun=my_Keister) # define transformed integrand
+>>> y = cf.f(cf.distribution.gen_samples(2**7)) # evaluate transformed integrand at Halton points
+>>> y.mean() # approximate the integral
 1.808723467976591
