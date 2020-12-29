79ad0c7ea9f6508e1f0001da442b79cd5d02ec16
fjhickernell <hickernell@iit.edu>
2020-12-29 14:48:18 -0600

    Updates from Overleaf

--- ags/python/tm_uniform.py
+++ ags/python/tm_uniform.py
@@ -1,6 +1,6 @@
 >>> u = qp.Uniform(qp.Halton(2), 
-            lower_bound = [-2,0], 
-            upper_bound = [2,4])
+...         lower_bound = [-2,0], 
+...         upper_bound = [2,4])
 >>> u.gen_samples(4)
 array([[ 0.85908285,  0.85683196],
        [-1.14091715,  2.19016529],
